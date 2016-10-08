f724516614fe25c69be96727a0fdc6486cf92efe
Brendan Costigan <Brendan Costigan>
2016-10-08 07:07:03 +0300

    Initial version

---
+++ AbstractClass/AbstractClassExample/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Brendan.AbstractClass
+{
+
+    public class AbstratcClass
+    {
+        public static void Main()
+        {
+            Fred fred = new AbstractClass.Fred();
+
+            fred.Count = 5;         // Note Count is in the abstract class
+
+            // Error from next line: Cannot create an instance of the abstract class or interface 'ConsoleApplication1.Person' when Person is an abstract class 
+            //Person p = new Person();
+
+            // If the method fred.SayVirtualHello is commented out then you get the output from SayVirtualHello in the abstract class Person.
+
+            Console.WriteLine(fred.SayVirtualHello());
+            Console.WriteLine(fred.Age());
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Fred: Person
+    {
+        //public string message;
+        public string SayVirtualHello()
+        {
+            return "I'm Fred";
+        }
+
+        // The person class has the abstract class Age which must be inherited by this class that inherits Person
+        // The override modifier is required to extend or modify the abstract or virtual implementation of an inherited method, property, indexer, or event.
+        public override int Age()
+        {
+            DateTime dateOfBirth = new DateTime(1980, 03, 25);
+
+            var today = DateTime.Today;
+            var age = today.Year - dateOfBirth.Year;
+            if (dateOfBirth > today.AddYears(-age)) age--;
+
+            return age;
+        }
+    }
+
+    // Use the abstract modifier in a class declaration to indicate that a class is intended only to be a base class of other classes. 
+    // Members marked as abstract, or included in an abstract class, must be implemented by classes that derive from the abstract class.
+
+    public abstract class Person                // Class must be abstract to contain an abstract method
+    {
+        public int Count;
+
+        public virtual string SayVirtualHello()          // The virtual keyword is used to modify a method, property, indexer, or event declaration and allow for it to be overridden in a derived class.
+        {
+            return  "I'm a person";
+        }
+
+        // Age cannot have a body becuase it is an abstract class and we are telling the inheriting class to implement an age method
+        public abstract int Age();
+
+    }
+}
---
+++ AbstractClass/AbstractClass_MSDN_Example/AbstractClass_MSDN_Example.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+// Source: https://msdn.microsoft.com/en-us/library/sf985hc5.aspx
+
+namespace AbstractClass_MSDN_Example
+{
+    abstract class ShapesClass
+    {
+        abstract public int Area();
+    }
+
+    class Square : ShapesClass
+    {
+        int side = 0;
+
+        public Square(int n)
+        {
+            side = n;
+        }
+        // Area method is required to avoid 
+        // a compile-time error. 
+        public override int Area()
+        {
+            return side * side;
+        }
+
+        static void Main()
+        {
+            Square sq = new Square(12);
+            Console.WriteLine("Area of the square = {0}", sq.Area());
+            Console.ReadKey();
+        }
+
+        interface I
+        {
+            void M();
+        }
+
+        abstract class C : I
+        {
+            public abstract void M();
+        }
+
+    }
+    // Output: Area of the square = 144
+}
